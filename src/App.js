ddc855615c022c551dd470ce8f3109924040a59a
muhammad asrlan <muhammad.arslan@denovers.com>
2021-02-18 15:48:22 +0500

    setup front-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import React from 'react'
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+React.useEffect(() => {
+ 
+  console.log('object')
+}, [])
+
+ const handleClick = async () => {
+   try{
+    const res = await axios.get('/getOrg')
+    console.log({res})
+   }catch(e){
+     console.log(e.message)
+   }
+   
+   console.log("clicked")
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick} > Get Data  </button>
     </div>
   );
 }
